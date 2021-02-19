7c0b1e401eda815d00ac95aff1860c9abc07b876
felphlk <57629494+felphlk@users.noreply.github.com>
2021-02-19 15:36:35 -0300

    Update Divisao.java

--- src/Divisao.java
+++ src/Divisao.java
@@ -2,7 +2,7 @@ public class Divisao extends Operacao {
     @Override
     public int efetuarOperacao(int x, int y) {
         if(y==0 || x==0 ){
-            System.out.println("Divisão: 0, operação invalida(Divisão por 0)");
+            System.out.println("Operação invalida(Divisão por 0)");
             return 0;
         }
         return x/y;
